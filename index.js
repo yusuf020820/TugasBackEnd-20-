4dc81b0e7c52b0ae1ed670950e7678125d680c0f
xxyannn <riyandiarnashhh@gmail.com>
2024-06-18 22:14:44 +0700

    updtateeeee

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use(express.json());
 app.use("/api", exampleRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`)
 });
